38c0aefd67c59ebdb08c1861aeba0cd9a0ff17ad
init050 <9hoh3n1ex@protonmail.com>
2024-11-25 00:28:59 +0330

    redis done

--- Plant_Shop/settings.py
+++ Plant_Shop/settings.py
@@ -141,3 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # mysite/settings.py
 # Daphne
 ASGI_APPLICATION = "Plant_Shop.asgi.application"
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
--- online_chat_module/consumers.py
+++ online_chat_module/consumers.py
@@ -1,17 +1,40 @@
-from channels.generic.websocket import WebsocketConsumer
 import json
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
-
     def connect(self):
-        self.accept()
-    def disconnect(self, close_code):
-        pass
-    def receive(self, text_data):
-        text_data_dict = json.loads(text_data)
-        message = text_data_dict['message']
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
 
-        self.send(text_data=json.dumps({
-            'message':message,
-        }))
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({"message": message}))
