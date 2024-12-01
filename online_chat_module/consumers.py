0f72d57689f98b93ea1393b56cc33cbca7caad59
init050 <9hoh3n1ex@protonmail.com>
2024-12-01 14:55:23 +0330

    serialized

--- online_chat_module/consumers.py
+++ online_chat_module/consumers.py
@@ -2,39 +2,65 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer 
+from .serializers import MessaageSerializers
+from .models import Message
+from rest_framework.renderers import JSONRenderer
 
 
 class ChatConsumer(WebsocketConsumer):
+
+    def new_message(self, data):
+        print('as')
+
+
+    def fetch_message(self):
+        qs = Message.last_message(self)
+        message_json = self.message_serializers(qs)
+        content = {
+            'message':eval(message_json)
+        }
+        self.chat_message(content)
+
+
+    def message_serializers(self, qs):
+        serialized = MessaageSerializers(qs, many=True)
+        content = JSONRenderer().render(serialized.data)
+        return content
+
+
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
-
         self.accept()
 
+
+    commands = {
+        'new_message':new_message,
+        'fetch_message':fetch_message,
+    }
+
+
     def disconnect(self, close_code):
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
-    # Receive message from WebSocket
+    
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        command = text_data_json['command']
+        self.commands[command](self)
 
-        # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat.message", "message": message}
         )
 
-    # Receive message from room group
+
     def chat_message(self, event):
         message = event["message"]
-
-        # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
--- online_chat_module/models.py
+++ online_chat_module/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from account_module.models import User
 
 # Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
+    content = models.TextField(verbose_name='Content')
+    timestamp= models.DateTimeField(auto_now_add=True, verbose_name='Date & Time')
+
+    def last_message(self):
+        return Message.objects.order_by('-timestamp').all()
+    
+    def __str__(self) -> str:
+        return self.author.username
