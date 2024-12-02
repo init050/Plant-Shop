35ed70bc5d77a302907ac3e3307c7301d1c7774b
init050 <9hoh3n1ex@protonmail.com>
2024-12-02 16:07:25 +0330

    install npm and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './myapp/templates/**/*.html',
+    './myapp/static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
