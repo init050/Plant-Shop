8d7a184a9d9097efb2952bcee3560d712a2ded78
init050 <9hoh3n1ex@protonmail.com>
2024-12-05 11:56:21 +0330

    setup tailwind

--- home_module/views.py
+++ home_module/views.py
@@ -6,3 +6,7 @@ from django.http import HttpRequest
 
 def indexhome(request):
     return render(request, 'home_module/index.html')
+
+
+def site_header_partial(request):
+    return render(request, 'shared/site_header_partial.html')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.html', './static/**/*.css'],
+  content: [
+    './templates/**/*.html',  
+    './**/templates/**/*.html', 
+    './static/js/**/*.js',   
+    './static/css/**/*.css',  
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        'ww':'1B2316',
+        secondary: '#64748B',
+      },
+    },
   },
   plugins: [],
 };
