11341bf19efaaa1bc03688c7fdecfdd63214e64e
init050 <9hoh3n1ex@protonmail.com>
2024-12-24 16:42:41 +0330

    edit style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html",        // تمام فایل‌های HTML در پوشه templates و زیرپوشه‌های آن
-    "./**/templates/**/*.html",     // برای هر ماژول که داخلش پوشه templates دارد
-    "./static/**/*.css",            // تمام فایل‌های CSS در پوشه static و زیرپوشه‌های آن
-    "./**/static/**/*.css",         // برای هر ماژول که داخلش پوشه static دارد
+    "./home_module/templates/**/*.html",
+    "./templates/**/*.html",
+    "./static/css/src/**/*.{html,js,css}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
