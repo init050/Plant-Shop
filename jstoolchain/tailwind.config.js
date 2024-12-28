b9c979da4d5e0d9d48afce3cd3083ad4790acb6d
init050 <9hoh3n1ex@protonmail.com>
2024-12-28 23:17:43 +0330

    style

---
+++ jstoolchain/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./home_module/templates/**/*.html",
+    "./templates/**/*.html",
+    "./static/css/src/**/*.{html,js,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
